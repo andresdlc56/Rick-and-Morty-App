5d300d005a867815c9bcc85dd3a73f9f667e18ff
Andres de la Cruz <andresdlc56@gmail.com>
2023-02-06 15:27:31 -0400

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "./components/Navbar"
+
+function App() {
+    return (
+      <Navbar title={ 'Rick and Morty App' } />
+    )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export const Navbar = ({ title }) => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand text-uppercase" href="/">{ title }</a>
+            </div>
+        </nav>
+    )
+}
